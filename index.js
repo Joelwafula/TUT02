3aa88c73c864f990c172d65f284ce66db975cfdc
Joel <joelwafula2020@gmail.com>
2024-07-08 07:29:06 +0300

    "writing files in node js"

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const path = require("path")
 const fileOps = async ()=>{
 
     try{
-        const data = await fsPromises.readFile
+        const data = await fsPromises.readFile(path.join(__dirname,'files','starter.txt'),'utf8');
+        console.log(data)
 
     }catch (err){
         console.log(err)
     }
 
 }
+
+fileOps();
 
 const oldPath = path.join(__dirname,'files','reply.txt')
 
@@ -23,25 +26,25 @@ fs.readFile(path.join(__dirname,"files","starter.txt"),"utf8",(err,data)=>{
     console.log(data)
 })
 
-fs.writeFile(path.join(__dirname,'files',"reply.txt"),"its nice to know you",(err)=>{
-    if(err) throw err;
+// fs.writeFile(path.join(__dirname,'files',"reply.txt"),"its nice to know you",(err)=>{
+//     if(err) throw err;
 
-    console.log("The write is complete")
+//     console.log("The write is complete")
 
-    fs.appendFile(path.join(__dirname,'files','reply.txt'),"\n\n yes it is",(err)=>{
-        if (err) throw err
+//     fs.appendFile(path.join(__dirname,'files','reply.txt'),"\n\n yes it is",(err)=>{
+//         if (err) throw err
 
-        console.log("File has been appended sucessfully")
-    })
+//         console.log("File has been appended sucessfully")
+//     })
 
-    fs.rename(oldPath,newPath,(err)=>{
-        if (err) throw err
+//     fs.rename(oldPath,newPath,(err)=>{
+//         if (err) throw err
 
-        console.log("File has been renamed sucessfully")
-    })
+//         console.log("File has been renamed sucessfully")
+//     })
 
 
-})
+// })
 
 fs.appendFile(path.join(__dirname,"files","test.txt"),"hello",(err)=>{
     if (err) throw err
